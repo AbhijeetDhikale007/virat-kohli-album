530243d008dde07662c51e05f422d6a9709bcff9
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-13 11:55:58 +0000

    Added: Carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,23 @@
-importing error 
+<script lang='ts'>
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+    import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
+    const slides = [
+        'Images1', 
+    ]
+</script>
+
+<Splide options={ { rewind: true } } aria-label="My Favorite Images">
+    { #each slides as slide }
+    <SplideSlide>
+      <img src={ slide.src } alt={ slide.alt }>
+    </SplideSlide>
+  { /each }
+</Splide>
+
+<style lang='scss'>
+    img {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
