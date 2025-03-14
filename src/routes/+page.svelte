db43dadf197d14272f78c8e2588dbf6da1722f12
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-14 08:18:28 +0000

    Improvement: Carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,38 @@
 <script lang='ts'>
     import { Splide, SplideSlide } from '@splidejs/svelte-splide';
     import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+    import Ico from '$lib/index.js'
+
+    import 'virtual:uno.css'
 
     const slides = [
         'Image1', 'Image2'
     ]
 </script>
 
-<Splide options={ { rewind: true } } aria-label="My Favorite Images">
+<Splide
+    options={ {
+        rewind: true,
+        type: 'fade',
+        autoplay: true,
+        keyboard: true,
+        wheel: true,
+        wheelMinThreshold: 30,
+        releaseWheel: true,
+        lazyLoad: true,
+        drag: true,
+        width: '100vw',
+        height: '100vh'
+    } }
+    aria-label="My Favorite Images">
+    <div class="splide__arrows">
+		<button class="splide__arrow splide__arrow--prev z-2">
+			<Ico name='Left' />
+		</button>
+		<button class="splide__arrow splide__arrow--next z-2">
+			<Ico name='Left' />
+		</button>
+  </div>
     {#each slides as slide, index }
     <SplideSlide>
       <img src='Images/Landscape/{ slide }.jpg' alt={ 'Picture'[index] }>
@@ -16,8 +41,10 @@
 </Splide>
 
 <style lang='scss'>
-    img {
-        width: 100vw;
-        height: 100vh;
-    }
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    @apply w-100% h-100%;
+  }
 </style>
