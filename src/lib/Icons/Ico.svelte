9f813c9d1939300d568acb38d2717116c7719e2c
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-03-14 08:18:06 +0000

    Created: Ico Component

---
+++ src/lib/Icons/Ico.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { Icons } from '$data/Icons.js'
+
+    export let name: keyof typeof Icons
+    let xmlns: string = 'http://www.w3.org/2000/svg'
+    let width: string = '100%'
+    let height: string = '100%'
+    let displayIcon = Icons[name];
+</script>
+
+<svg
+    class={$$props.class}
+    {xmlns}
+    {width}
+    {height}
+    viewBox={displayIcon.viewBox}>{@html displayIcon.svg}
+</svg>
